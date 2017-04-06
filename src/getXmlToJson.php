45754f6fb165feb5eb2fea272955d9ce0d7ec33e
yama-dev <tatsuya.yamamoto69@gmail.com>
2017-04-06 23:04:50 +0900

    ステータスコードの仕様変更、コメントアウトの追記

--- src/getXmlToJson.php
+++ src/getXmlToJson.php
@@ -1,13 +1,30 @@
 <?php
 
-/* http_response_code(200); */
+/**
+ * php-RSS_to_jsonp-module PHP Library v1.0
+ * https://github.com/yama-dev/php-RSS_to_jsonp-module
+ * Copyright yama-dev
+ * Licensed under the MIT license.
+ * Date: 2016-11-27
+ *
+ * RSSの取得をして、json or jsonpを出力するスクリプト
+ * @return $rss_data|str
+ */
+
+date_default_timezone_set('Asia/Tokyo');
 
 $siteUrl = null;
 
 if (isset($_GET['url'])) {
+  if(preg_match('/\?/', $_GET['url'])):
+    $siteUrl = $_GET['url'] . '&' . $_SERVER['REQUEST_TIME'];
+  else:
     $siteUrl = $_GET['url'] . '?' . $_SERVER['REQUEST_TIME'];
+  endif;
+  header("HTTP/1.1 200 OK");
 } else {
-    exit('error');
+  header("HTTP/1.1 400 Bad Request");
+  exit('error');
 }
 
 /* Get RSS */
@@ -17,7 +34,7 @@ $rss_data  = preg_replace( "/<([^>]+?):(.+?)>/", "<$1_$2>", $rss_data );
 $rss_data  = simplexml_load_string( $rss_data, 'SimpleXMLElement', LIBXML_NOCDATA );
 $rss_array = array();
 foreach ( $rss_data->channel->item as $item ) {
-    $rss_array[] = $item;
+  $rss_array[] = $item;
 }
 
 /* OUTPUT */
--- src/getjson.php
+++ src/getjson.php
@@ -1,4 +1,16 @@
 <?php
+
+/**
+ * php-RSS_to_jsonp-module PHP Library v1.0
+ * https://github.com/yama-dev/php-RSS_to_jsonp-module
+ * Copyright yama-dev
+ * Licensed under the MIT license.
+ * Date: 2016-11-27
+ *
+ * jsonのクロスドメイン対策用スクリプト
+ * @return $json|str
+ */
+
 if(isset($_GET["url"])){
 	$json = file_get_contents( $_GET["url"] );
 	header( 'Content-Type: text/javascript; charset=utf-8' );
